78bf6a0e4a37942049f19d8bc906c7a62bce7963
hiep <hiep386703@gmail.com>
2023-03-26 10:30:13 +0700

    javascript-basic

---
+++ it.js
@@ -0,0 +1,30 @@
+console.log('Hello World')
+
+// const a ="..." là 1 biến lưu dữ liệu không bao giờ bị thay đổi 
+// let b ="..." là 1 biến lưu dữ liệu có thể được thay đổi
+// var c ="..." là 1 biến lưu dữ liệu có thể được thay đổi
+// let b và var c khác nhau về scope
+
+// data types (kiểu dữ liệu)
+// string ="task"
+// number =1,2,3....
+// object ={tệp}
+// boolean là 2 kiểu dữ liệu true or false
+// null(là kiểu dữ liệu của biến có già trị trống),undefined(là kiểu dữ liệu chưa gắn giá trị cho biến)
+
+const name =" Nhoc "
+const number = 3
+const obj = {
+    Firstname : " Hiep ",
+Lastname : " Pham ",
+Age : 26,
+Male : true,
+}
+
+let undefinedtest;
+let nulltest = null;
+
+// console.log(...) là câu lệnh để lưu lại giá trị của biến
+console.log(name)
+console.log(number)
+console.log({obj, undefinedtest, nulltest})
